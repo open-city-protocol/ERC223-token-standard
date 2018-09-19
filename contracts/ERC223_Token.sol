c16237b6bd513f505a4181a08ba5e7f0aa0bb98d
Justin Eichel <jeichel@miovision.com>
2018-09-19 11:43:57 -0400

    Fix ERC223 GH-51 Custom Fallback

--- contracts/ERC223_Token.sol
+++ contracts/ERC223_Token.sol
@@ -67,7 +67,7 @@ contract ERC223Token is ERC223, SafeMath {
         if (balanceOf(msg.sender) < _value) revert();
         balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
         balances[_to] = safeAdd(balanceOf(_to), _value);
-        assert(_to.call.value(0)(bytes4(keccak256(abi.encodePacked(_custom_fallback))), msg.sender, _value, _data));
+        assert(_to.call.value(0)(abi.encodeWithSignature(_custom_fallback, msg.sender, _value, _data)));
         emit Transfer(msg.sender, _to, _value, _data);
         return true;
     }
