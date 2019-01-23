450ae0d7badee2b3c238b087a92579c5fbac4688
Justin Eichel <jeichel@miovision.com>
2019-01-23 16:28:01 -0500

    fix(codebuild): remove unneeded provider from tuffle.js

--- truffle.js
+++ truffle.js
@@ -7,8 +7,6 @@ let mnemonic
 if (fs.existsSync('secrets.json')) {
   secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'))
   mnemonic = secrets.mnemonic
-
-  const provider = new HDWalletProvider(mnemonic, '');
 } else {
   console.log('No secrets.json found. If you are trying to publish EPM ' +
               'this will fail. Otherwise, you can ignore this message!')
